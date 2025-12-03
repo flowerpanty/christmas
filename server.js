2da9d54a49dd701990c202f401e1079c3405d04a
flowerpanty <flowerpanty@gmail.com>
2025-12-03 20:55:50 +0900

    fix: add newline between last two lines and restore buttons

--- server.js
+++ server.js
@@ -32,28 +32,28 @@ app.post('/api/send-kakao', async (req, res) => {
 
         // 메시지 구성
         // 메시지 구성 (템플릿과 정확히 일치해야 함)
-        const message = `[낫띵메터스]\n\n주문 접수 안내드립니다.\n\n고객명: ${name}\n주문내역: ${productSummary}\n수령방법: ${pickupMethod}\n수령일시: ${pickupDate} ${pickupTime}\n총금액: ${totalPrice}원\n\n주문하신 제품은 안내드린 일정에 맞추어 준비해드립니다채널 추가하고 이 채널의 광고와 마케팅 메시지를 카카오톡으로 받기`;
+        const message = `[낫띵메터스]\n\n주문 접수 안내드립니다.\n\n고객명: ${name}\n주문내역: ${productSummary}\n수령방법: ${pickupMethod}\n수령일시: ${pickupDate} ${pickupTime}\n총금액: ${totalPrice}원\n\n주문하신 제품은 안내드린 일정에 맞추어 준비해드립니다\n채널 추가하고 이 채널의 광고와 마케팅 메시지를 카카오톡으로 받기`;
 
         // 전화번호 포맷팅
         const phoneNumber = phone.replace(/[^0-9]/g, '');
 
-        // 버튼 정보 - 템플릿에 버튼이 없을 수도 있어 일단 제거
-        // const buttonInfo = {
-        //     "button": [
-        //         {
-        //             "name": "채널추가",
-        //             "linkType": "AC",
-        //             "linkTypeName": "채널 추가"
-        //         },
-        //         {
-        //             "name": "더 많은 귀여움 보러가기",
-        //             "linkType": "WL",
-        //             "linkTypeName": "웹링크",
-        //             "linkMo": "https://nothingmatters.co.kr/",
-        //             "linkPc": "https://nothingmatters.co.kr/"
-        //         }
-        //     ]
-        // };
+        // 버튼 정보
+        const buttonInfo = {
+            "button": [
+                {
+                    "name": "채널추가",
+                    "linkType": "AC",
+                    "linkTypeName": "채널 추가"
+                },
+                {
+                    "name": "더 많은 귀여움 보러가기",
+                    "linkType": "WL",
+                    "linkTypeName": "웹링크",
+                    "linkMo": "https://nothingmatters.co.kr/",
+                    "linkPc": "https://nothingmatters.co.kr/"
+                }
+            ]
+        };
 
         // 알리고 API 요청 파라미터
         const params = new URLSearchParams({
@@ -65,8 +65,8 @@ app.post('/api/send-kakao', async (req, res) => {
             'receiver_1': phoneNumber,
             'recvname_1': name,
             'subject_1': '주문 접수 안내',
-            'message_1': message
-            // 'button_1': JSON.stringify(buttonInfo) // 버튼 제거
+            'message_1': message,
+            'button_1': JSON.stringify(buttonInfo)
         });
 
         // Axios 설정 (IPv4 강제 사용)
